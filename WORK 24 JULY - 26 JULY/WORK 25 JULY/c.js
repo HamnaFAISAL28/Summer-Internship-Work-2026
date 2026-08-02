0d09b4dea4bebb73de5a7fd83b7573ecb5ec669f
HamnaFAISAL28 <hzmfr444@gmail.com>
2026-08-03 00:34:00 +0500

    Update c.js

--- WORK 24 JULY - 26 JULY/WORK 25 JULY/c.js
+++ WORK 24 JULY - 26 JULY/WORK 25 JULY/c.js
@@ -1,4 +1,4 @@
-/*
+
 console.log(document.getElementsByName('span'))
 
 console.log(document.getElementsByName('span'))
@@ -14,17 +14,17 @@ document.body.firstChild.nodeName
 document.body.firstElementChild
 
 console.log(document.body.firstElementChild)
-*/
 
-/* let x=document.getElementsByTagName('span')[0]
+
+ let x=document.getElementsByTagName('span')[0]
 console.log(x)
 
 let y=document.getElementsByTagName('span')[0]
-console.dir(y) */
+console.dir(y)
 
-/* first.innerHTML
+first.innerHTML
 
-first.innerHTML="<i>Hey I am span</i>" */
+first.innerHTML="<i>Hey I am span</i>" 
 
 first.outerHTML
 
