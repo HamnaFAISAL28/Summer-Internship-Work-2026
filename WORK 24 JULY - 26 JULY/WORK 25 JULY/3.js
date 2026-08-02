074c7309b691bd5b5223f5cba9715d0fae0e7f0f
HamnaFAISAL28 <hzmfr444@gmail.com>
2026-08-03 00:29:37 +0500

    Add files via upload

---
+++ WORK 24 JULY - 26 JULY/WORK 25 JULY/3.js
@@ -0,0 +1,16 @@
+ let a=first.getAttribute("class")
+console.log(a)
+
+console.log(first.hasAttribute("class"))
+
+console.log(first.hasAttribute("style"))
+
+ first.setAttribute("hidden","true")
+
+console.log(first.setAttribute("class","true hamna"))
+
+console.log(first.removeAttribute("class"))
+
+ console.log(first.attributes) 
+
+console.log(first.dataset)
