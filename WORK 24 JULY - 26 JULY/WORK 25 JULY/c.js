c1a1f17bc01e471c7575c30dfa0cedaffa883400
HamnaFAISAL28 <hzmfr444@gmail.com>
2026-08-03 00:07:42 +0500

    Add files via upload

---
+++ WORK 24 JULY - 26 JULY/WORK 25 JULY/c.js
@@ -0,0 +1,36 @@
+/*
+console.log(document.getElementsByName('span'))
+
+console.log(document.getElementsByName('span'))
+
+console.log(document.getElementsByName('span'))[0]
+
+console.log(document.getElementsByTagName('span'))[0]
+
+console.dir(document.getElementsByName('span'))[0]
+
+document.body.firstChild.nodeName
+
+document.body.firstElementChild
+
+console.log(document.body.firstElementChild)
+*/
+
+/* let x=document.getElementsByTagName('span')[0]
+console.log(x)
+
+let y=document.getElementsByTagName('span')[0]
+console.dir(y) */
+
+/* first.innerHTML
+
+first.innerHTML="<i>Hey I am span</i>" */
+
+first.outerHTML
+
+
+first.outerHTML="<div>Hey</div>"
+ 
+
+document.body.firstChild
+
