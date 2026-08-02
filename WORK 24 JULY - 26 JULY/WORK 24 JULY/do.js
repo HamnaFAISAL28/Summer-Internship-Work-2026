8f65143afa3e53cb80254c902b2e59d12a99d068
HamnaFAISAL28 <hzmfr444@gmail.com>
2026-08-02 20:56:56 +0500

    Add files via upload

---
+++ WORK 24 JULY - 26 JULY/WORK 24 JULY/do.js
@@ -0,0 +1,23 @@
+//let ctitle=document.getElementsByClassName("card-title")[0]
+//ctitle.style.color="red"
+
+//let ctitle=document.getElementById("firstclasstitle")[0]
+//ctitle.style.color="red"
+
+//let ctitle = document.getElementById("firstclasstitle");
+//ctitle.style.color = "red";
+
+let ctitles=document.querySelectorAll(".card-title")
+ctitles[0].style.color="red"
+ctitles[1].style.color="blue"
+ctitles[2].style.color="green"
+console.log(ctitles)
+
+document.querySelector(".this").style.backgroundColor="yellow"
+document.querySelector(".this").style.color="black"
+
+document.getElementsByTagName("a")
+console.log(document.body.getElementsByTagName("a"))
+console.log(document.querySelector(".card").getElementsByTagName("a"))
+
+console.log(document.getElementsByName("search"))
