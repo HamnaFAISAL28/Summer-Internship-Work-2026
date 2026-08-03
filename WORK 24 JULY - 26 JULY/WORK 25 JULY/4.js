c941d5bdd3871470b5dbb3bc31e0ab848ce5ef6e
HamnaFAISAL28 <hzmfr444@gmail.com>
2026-08-04 01:12:52 +0500

    Update 4.js

--- WORK 24 JULY - 26 JULY/WORK 25 JULY/4.js
+++ WORK 24 JULY - 26 JULY/WORK 25 JULY/4.js
@@ -1,6 +1,6 @@
 let a= document.getElementsByTagName('div')[0] 
 
-/* a.innerHTML=a.innerHTML+'<h1>Hello World!</h1>'; */
+a.innerHTML=a.innerHTML+'<h1>Hello World!</h1>'; 
 
 
 let div=document.createElement('div');
