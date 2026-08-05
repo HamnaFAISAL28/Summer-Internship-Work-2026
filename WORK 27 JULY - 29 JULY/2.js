868068561427aad3f8fc38ead0fcd9d030669336
HamnaFAISAL28 <hzmfr444@gmail.com>
2026-08-06 01:46:44 +0500

    Add files via upload

---
+++ WORK 27 JULY - 29 JULY/2.js
@@ -0,0 +1,57 @@
+function goodMorning(error,src) {
+    if(error)
+    console.log(error)
+sendEmergencyMessage();
+return
+}
+alert("Good Morning"+ src)
+
+
+
+loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js",function goodMorning(error,src)
+{
+
+
+    if(error)
+    {
+       console.log(error)
+       sendEmergencyMessage();
+       return
+    }
+
+    alert("Good Morning"+src)
+})
+
+
+loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js",function goodMorning(error,src)
+{
+
+
+    if(error)
+    {
+       console.log(error)
+       sendEmergencyMessage();
+       return
+    }
+
+   loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap2.bundle.min.js",function goodMorning(error,src)
+   {
+      
+    if(error)
+    {
+       console.log(error)
+       sendEmergencyMessage();
+       return
+    }
+
+   loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap3.bundle.min.js",function goodMorning(error,src)
+   {}
+   
+})
+})
+})
+
+
+
+
+
